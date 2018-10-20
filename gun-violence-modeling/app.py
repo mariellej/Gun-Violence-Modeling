c8b561c765ef121754527b3c2d32bb7e4d72abd7
JustinJoco <justin.joco@gmail.com>
2018-10-20 16:18:06 -0400

    Added US map

--- gun-violence-modeling/app.py
+++ gun-violence-modeling/app.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, jsonify
 from stock_scraper import get_data
-
+import json
+import pandas as pd
 app = Flask(__name__)
 
 
 @app.route("/")
-def hell():
+def hello():
     return render_template("index.html")
 
 @app.route("/data")
 def data():
-    return jsonify(get_data())
 
+    with open('data1000.json') as file:
+        json_data = json.load(file)
+    return jsonify(json_data)
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
