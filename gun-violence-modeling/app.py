6b1c3cfa9d29af73e2198871d1f153bac214929e
JustinJoco <justin.joco@gmail.com>
2018-10-20 23:20:39 -0400

    Added data visuals

--- gun-violence-modeling/app.py
+++ gun-violence-modeling/app.py
@@ -9,14 +9,19 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
-@app.route("/data")
-def data():
+@app.route("/incident_data")
+def incident_data():
 
     with open('data1000.json') as file:
         json_data = json.load(file)
     return jsonify(json_data)
 
+@app.route("/us_states")
+def us_data():
+    with open('us-states.json') as us_file:
+        us_json_data = json.load(us_file)
+    return jsonify(us_json_data)
+
 
 if __name__ == "__main__":
-
     app.run(debug=True)
